e2eaab4411663daa1b2641734ad74eb5e609161d
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-10-05 13:19:40 -0700

    add fake data test generator and test.

---
+++ Extras/Tests/ClassifModelsFakeDataTest.R
@@ -0,0 +1,28 @@
+#' ### Classification Model Tests
+library(testthat)
+library(plyr)
+source('Supporting_Macros/CrossValidation1.r')
+
+config <- list(
+  classification = TRUE,
+  displayGraphs = FALSE,
+  numberFolds = 5,
+  numberTrials = 3,
+  posClass = "",
+  regression = FALSE,
+  stratified = FALSE,
+  targetField = 'lastCol'
+)
+
+payload <- makePayload(
+  'Extras/Tests/Data/FakeData.csv',
+  'Extras/Tests/Data/RandomForest_FakeData.rds',
+  config$targetField
+)
+
+set.seed(1234)
+inputs <- list(data = payload$data, models = payload$models[1])
+d <- runCrossValidation(inputs, config)
+
+#l_ply(names(payload$models), failwith(f = runTest), payload = payload)
+
